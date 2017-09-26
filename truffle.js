2d87062c2085ee4ff37ea776adce5d269d518d7d
vrolland <vincent.rolland.38@gmail.com>
2017-09-26 11:34:35 +0200

    truffle config

--- truffle.js
+++ truffle.js
@@ -1,5 +1,16 @@
 module.exports = {
   networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
+
+
+module.exports = {
+  networks: {
     "live": {
       network_id: 1,
       host: "localhost",
@@ -12,9 +23,10 @@ module.exports = {
       gas: 4000000,
       from: "0xaccdd9bac6fdd4e730f183b9e450a0ec6fd8e3c7",
     },
-  },
   rpc: {
     host: "localhost",
-    port: 8545
+    port: 8545,
+    network_id: "*" // Match any network id
+  }
   }
 };
