800a543586ed57aa98fcff8eb92d6b4188273184
vrolland <vincent.rolland.38@gmail.com>
2017-09-27 18:05:07 +0200

    RequestToken : add burn fonction

--- contracts/RequestToken.sol
+++ contracts/RequestToken.sol
@@ -22,6 +22,12 @@ contract RequestToken is StandardToken, Ownable {
         _;
     }
 
+    modifier validDestination( address to ) {
+        require(to != address(0x0));
+        require(to != address(this) );
+        _;
+    }
+
     function RequestToken( uint tokenTotalAmount, uint _transferableStartTime, address _admin, address _earlyInvestorWallet) {
         // Mint all tokens. Then disable minting forever.
         totalSupply = tokenTotalAmount * (10 ** uint256(decimals));
@@ -38,6 +44,7 @@ contract RequestToken is StandardToken, Ownable {
 
     function transfer(address _to, uint _value)
         public
+        validDestination(_to)
         onlyWhenTransferEnabled
         returns (bool) {
         return super.transfer(_to, _value);
@@ -45,9 +52,31 @@ contract RequestToken is StandardToken, Ownable {
 
     function transferFrom(address _from, address _to, uint _value)
         public
+        validDestination(_to)
         onlyWhenTransferEnabled
         returns (bool) {
         return super.transferFrom(_from, _to, _value);
+    }
+
+    event Burn(address indexed _burner, uint _value);
+
+    function burn(uint _value) 
+        public
+        onlyWhenTransferEnabled
+        returns (bool)
+    {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(msg.sender, _value);
+        Transfer(msg.sender, address(0x0), _value);
+        return true;
+    }
+
+    // save some gas by making only one contract call
+    function burnFrom(address _from, uint256 _value) onlyWhenTransferEnabled
+        returns (bool) {
+        assert( transferFrom( _from, msg.sender, _value ) );
+        return burn(_value);
     }
 
     function emergencyERC20Drain( ERC20 token, uint amount ) 
