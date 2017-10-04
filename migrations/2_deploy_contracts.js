d7320bbcfb2588f63dd8b051c6c9bb8f34b390c7
vrolland <vincent.rolland.38@gmail.com>
2017-10-04 10:58:36 +0200

    deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,26 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
+var RequestTokenSale = artifacts.require("./RequestTokenSale.sol");
+
+
+// Copy & Paste this
+Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
+if(!Date.now) Date.now = function() { return new Date(); }
+Date.time = function() { return Date.now().getUnixTime(); }
+
+
+var tokenSaleContract;
 
 module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
+    var publicSaleStartTime = new Date("Fri, 13 Oct 2017 07:00:00 GMT").getUnixTime();
+    var publicSaleEndTime = new Date("Tue, 17 Oct 2017 07:00:00 GMT").getUnixTime();
+
+	console.log( "#################################################################################");
+	console.log( "publicSaleStartTime : "+publicSaleStartTime);
+	console.log( "publicSaleStartTime : "+new Date(publicSaleStartTime*1000));
+	console.log( "publicSaleEndTime : "+publicSaleEndTime);
+	console.log( "publicSaleEndTime : "+new Date(publicSaleEndTime*1000));
+	console.log( "#################################################################################");
+    return RequestTokenSale.new(publicSaleStartTime, publicSaleEndTime).then(function(result){
+        tokenSaleContract = result;
+        console.log("RequestTokenSale: "+tokenSaleContract.address);
+    });
 };
