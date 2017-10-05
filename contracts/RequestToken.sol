29c7ae81fcbba906ec59082e56135750c1c14353
vrolland <vincent.rolland.38@gmail.com>
2017-10-05 10:19:36 +0200

    Fix compilator : 0.4.15

--- contracts/RequestToken.sol
+++ contracts/RequestToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.13;
+pragma solidity 0.4.15;
 
 import './base/token/StandardToken.sol';
 import './base/ownership/Ownable.sol';
--- contracts/RequestTokenSale.sol
+++ contracts/RequestTokenSale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.15;
 
 import "./base/crowdsale/CappedCrowdsale.sol";
 import "./WhitelistedCrowdsale.sol";
